fdf00e31cd6231ef7853b9bcbae74f724729c989
jh <oluashegun@googlemail.com>
2020-10-12 09:53:28 +0100

    crossfit links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,36 @@ class PagesController < ApplicationController
   def home
   end
 
-  def about
+  def aboutus
+  end
+
+  def crossfittonbridge
+  end
+
+  def crossfittag
+  end
+
+  def successess
+  end
+
+  def theteam
+  end
+
+  def clubrecords
+  end
+
+  def blog
+  end
+
+  def competitions
+  end
+
+  def kidsfit
+  end
+
+  def wod
+  end
+
+  def youtubevideos
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'about', to: 'pages#about', as: :about
+  get 'aboutus', to: 'pages#aboutus'
+  get 'crossfittonbridge', to: 'pages#crossfittonbridge'
+  get 'crossfittag', to: 'pages#crossfittag'
+  get 'successess', to: 'pages#successess'
+  get 'theteam', to: 'pages#theteam'
+  get 'clubrecords', to: 'pages#clubrecords'
+  get 'blog', to: 'pages#blog'
+  get 'competitions', to: 'pages#competitions'
+  get 'kidsfit', to: 'pages#kidsfit'
+  get 'wod', to: 'pages#wod'
+  get 'youtubevideos', to: 'pages#youtubevideos'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
